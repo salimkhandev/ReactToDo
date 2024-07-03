fd28927c95b59c1cda9897f70276ded4b468dffd
Salim Khan <salim@gmail.com>
2024-07-04 00:29:00 +0500

    some new changes made

--- indexx.js
+++ indexx.js
@@ -1,6 +1,7 @@
-const person = {
-  name: "John",
-  age: 30,
-  occupation: "Engineer"
-};
-console.log(!person['ag'])
+const fruits = ["apple", "banana", "orange", "banana", "grape"];
+
+const index = fruits.indexOf("banana");
+console.log(index); // Output: 1
+
+const index2 = fruits.indexOf("kiwi");
+console.log(index2); // Output: -1
--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,46 @@
-import { useState, useEffect } from "react";
-
-import Button from "@mui/material/Button";
-import SearchIcon from "@mui/icons-material/Search";
-import "./App.css"; // Make sure to import your CSS file
-import Alert from "@mui/material/Alert";
-import DarkModeIcon from "@mui/icons-material/DarkMode";
-import WbSunnyIcon from "@mui/icons-material/WbSunny";
-import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
-import ClearIcon from "@mui/icons-material/Clear";
-import Swal from "sweetalert2";
-import { red } from "@mui/material/colors";
 import AddTaskIcon from "@mui/icons-material/AddTask";
+import ClearIcon from "@mui/icons-material/Clear";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+import EditIcon from "@mui/icons-material/Edit";
+import SaveIcon from "@mui/icons-material/Save";
+import SearchIcon from "@mui/icons-material/Search";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
+import { IconButton, TextField } from "@mui/material";
+import Alert from "@mui/material/Alert";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import { useEffect, useState } from "react";
+import Swal from "sweetalert2";
+import "./App.css"; // Make sure to import your CSS file
+import { red } from "@mui/material/colors";
+
 export default function App() {
   const [notes, setNotes] = useState([]);
   const [check, setCheck] = useState();
   const [search, setSearch] = useState("");
   const [darkMode, setDarkMode] = useState(false);
   const [reset, setReset] = useState(false);
-   const [completedNotes, setCompletedNotes] = useState({});
-   
+
+  const [completedNotes, setCompletedNotes] = useState({});
+  const [editing, setEditing] = useState(null);
+  const [editText, setEditText] = useState("");
+  const [snackbar, setSnackbar] = useState({ open: false, message: "" });
+  const [showCompleted, setShowCompleted] = useState(false);
 
   useEffect(() => {
-
-
-
     const locals = localStorage.getItem("ReactTodo");
     const checkCompleted = localStorage.getItem("checkCompleted");
     if (locals) {
       const conToArray = locals.split(",");
-
       setNotes(conToArray);
     }
     if (checkCompleted) {
-  setCompletedNotes(JSON.parse(checkCompleted))
-}
-
+      setCompletedNotes(JSON.parse(checkCompleted));
+    }
   }, []);
 
-  const Handlereset=()=>{
+  const Handlereset = () => {
     Swal.fire({
       title: "Are you sure?",
       text: "You won't be able to revert this!",
@@ -56,67 +59,62 @@ export default function App() {
         Swal.fire("Deleted!", "Your item has been deleted.", "success");
         localStorage.removeItem("ReactTodo");
         localStorage.removeItem("checkCompleted");
-        setCompletedNotes("");
+        setCompletedNotes({});
         setNotes([]);
         setReset(true);
       }
     });
-    
-  }
+  };
 
-  const onDelete = (note,key) => {
+  const onDelete = (note, key) => {
     const deletes = notes.filter((item) => item !== note);
     setNotes(deletes);
 
-
     localStorage.setItem("ReactTodo", deletes.join(","));
-     setCompletedNotes((prev) => {
+    setCompletedNotes((prev) => {
       const updateState = {
-      ...prev,
-      [key]: false,
+        ...prev,
+        [key]: false,
+      };
 
-    }
+      localStorage.setItem("checkCompleted", JSON.stringify(updateState));
+      return updateState;
+    });
 
-    localStorage.setItem('checkCompleted', JSON.stringify(updateState))
-    return updateState
-  })
-
-  }
-
-
-
+    setSnackbar({ open: true, message: "Note deleted" });
+  };
 
   const handleCheckboxChange = (key) => {
     setCompletedNotes((prev) => {
       const updateState = {
-      ...prev,
-      [key]: !prev[key],
+        ...prev,
+        [key]: !prev[key],
+      };
 
-    }
-
-    console.log(updateState); 
-    localStorage.setItem('checkCompleted', JSON.stringify(updateState))
-    return updateState
-  }
-
-  );
-
-
+      localStorage.setItem("checkCompleted", JSON.stringify(updateState));
+      return updateState;
+    });
   };
 
   const handleChange = (e) => {
     setSearch(e.target.value);
-      // setReset(true);
   };
 
   const filtered = notes.filter((item) => {
+    if (showCompleted) {
+      return (
+        completedNotes[notes.indexOf(item)] &&
+        item.toLowerCase().includes(search.toLowerCase())
+      );
+    }
     return item.toLowerCase().includes(search.toLowerCase());
   });
+  console.log('The filtered items are ',filtered);
 
   const toSubmit = (e) => {
     e.preventDefault();
     const value = e.target[0].value.trim();
-    
+
     if (notes.includes(value)) {
       setCheck("Note already exists!");
       setTimeout(() => {
@@ -127,13 +125,33 @@ export default function App() {
       setReset(false);
       localStorage.setItem("ReactTodo", [...notes, value].join(","));
       e.target[0].value = "";
+      setSnackbar({ open: true, message: "Note added" });
     } else {
-      setCheck("cannot be empy!");
+      setCheck("Cannot be empty!");
       setTimeout(() => {
         setCheck("");
       }, 2000);
     }
   };
+
+  const handleEdit = (key) => {
+    setEditing(key);
+    setEditText(notes[key]);
+  };
+
+  const handleSave = (key) => {
+    const updatedNotes = [...notes];
+    updatedNotes[key] = editText;
+    setNotes(updatedNotes);
+    localStorage.setItem("ReactTodo", updatedNotes.join(","));
+    setEditing(null);
+    setSnackbar({ open: true, message: "Note edited" });
+  };
+
+  const handleCloseSnackbar = () => {
+    setSnackbar({ open: false, message: "" });
+  };
+
   return (
     <div
       className={`${
@@ -142,7 +160,7 @@ export default function App() {
     >
       <div className="min-h-screen  flex items-center justify-center p-4">
         <div
-          className={` p-6 rounded-lg shadow-lg w-full max-w-md ${
+          className={`p-6 rounded-lg shadow-lg w-full max-w-md ${
             darkMode ? `bg-gray-800  text-white` : `bg-gray-100 text-black`
           }`}
         >
@@ -160,7 +178,7 @@ export default function App() {
             onClick={() => {
               setDarkMode(!darkMode);
             }}
-            className="pl-2  float-right"
+            className="pl-2 float-right"
           ></Button>
           <h1
             style={{ fontFamily: "'Saira Condensed', sans-serif" }}
@@ -168,12 +186,11 @@ export default function App() {
           >
             TaskTame
           </h1>
-
           <form onSubmit={toSubmit} className="mb-4">
             <div className="flex items-center mb-2">
               <input
                 type="text"
-                className={`flex-1 p-2 border  rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+                className={`flex-1 p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 ${
                   darkMode ? `text-black` : `text-black`
                 }`}
                 placeholder="Add a new note"
@@ -205,8 +222,7 @@ export default function App() {
             </Alert>
           )}
           <Button
-            // variant="outlined"
-            // color="error"
+            size="small"
             startIcon={<ClearIcon />}
             onClick={Handlereset}
             sx={{
@@ -217,20 +233,43 @@ export default function App() {
               color: "#d32f2f",
               "&:hover": {
                 backgroundColor: "#ef5350",
-                color: "black", // equivalent to Tailwind's bg-red-600
+                color: "black",
               },
               border: "1px solid red",
             }}
           >
             Clear all notes
           </Button>
+
+          <Button
+            style={{ margin: "6px" }}
+            sx={{
+              display: reset || notes.length === 0 ? "none" : "inline-flex",
+              px: 1,
+              py: 1,
+              borderRadius: "0.5rem",
+              color: "green",
+              "&:hover": {
+                backgroundColor: "#66bb6a",
+                color: "black",
+              },
+              border: "1px solid green",
+            }}
+            variant="outlined"
+            color="success"
+            onClick={() => {
+              setShowCompleted(!showCompleted);
+            }}
+          >
+            {showCompleted ? "Show all" : "Show Completed"}
+          </Button>
           <ul>
             {filtered.map((note, key) => (
               <li
-                className="flex justify-between items-center  p-2 border-b last:border-b-0"
+                className="flex justify-between items-center p-2 border-b last:border-b-0"
                 key={key}
               >
-                <div className=" flex">
+                <div className="flex">
                   <span className="pr-1">{key + 1}</span>
                   <input
                     type="checkbox"
@@ -239,29 +278,68 @@ export default function App() {
                     onChange={() => handleCheckboxChange(key)}
                   />
                 </div>
-                <span
-                  className={` flex-auto min-w-0 max-w-full break-words border border-gray-300 m-1 p-2  ${
-                    completedNotes[key]
-                      ? "line-through font-bold text-green-500"
-                      : ""
-                  }`}
-                >
-                  {note}
-                </span>
+                {editing === key ? (
+                  <TextField
+                    value={editText}
+                    onChange={(e) => setEditText(e.target.value)}
+                    size="small"
+                    sx={{ flex: 1, marginRight: 2}}
+                    InputProps={{
+                      style: {
+                        color: darkMode? "white":"black", // This will also change the input text color
+                      border:'2px solid green'
+                      },
 
-                <div className=" flex">
-                  <Button
-                    variant="text"
-                    style={{ color: "red", textTransform: "capitalize" }}
-                    onClick={() => onDelete(note, key)}
-                    startIcon={<DeleteOutlineOutlinedIcon />}
+
+                    }}
+                  />
+                ) : (
+                  <span
+                    className={`flex-auto min-w-0 max-w-full break-words border border-gray-300 m-1 p-2 ${
+                      completedNotes[key]
+                        ? "line-through font-bold text-green-500"
+                        : ""
+                    }`}
                   >
-                    Delete
-                  </Button>
+                    {note}
+                  </span>
+                )}
+                <div className="flex">
+                  {editing === key ? (
+                    <IconButton
+                      onClick={() => handleSave(key)}
+                      size="small"
+                      sx={{ color: "#4caf50" }}
+                    >
+                      <SaveIcon />
+                    </IconButton>
+                  ) : (
+                    <IconButton
+                      onClick={() => handleEdit(key)}
+                      size="small"
+                      sx={{ color: "#1976d2" }}
+                    >
+                      <EditIcon />
+                    </IconButton>
+                  )}
+                  <IconButton
+                    onClick={() => onDelete(note, key)}
+                    size="small"
+                    sx={{ color: "#f44336" }}
+                  >
+                    <DeleteOutlineOutlinedIcon />
+                  </IconButton>
                 </div>
               </li>
             ))}
           </ul>
+          <Snackbar
+            open={snackbar.open}
+            autoHideDuration={3000}
+            onClose={handleCloseSnackbar}
+            message={snackbar.message}
+            anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+          />
         </div>
       </div>
     </div>
