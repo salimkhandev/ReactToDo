d5ae3066b7b60dfa30cd029842e23f9e59a1f1e2
salimkhandev <salimt450s@gmail.com>
2024-07-08 19:43:04 +0500

    "Deleted files: index2.html, indexx.js, item, src/App2.jsx, src/assets/react.svg, and src/component/Child.jsx"

--- indexx.js
+++
@@ -1,7 +0,0 @@
-const fruits = ["apple", "banana", "orange", "banana", "grape"];
-
-const index = fruits.indexOf("banana");
-console.log(index); // Output: 1
-
-const index2 = fruits.indexOf("kiwi");
-console.log(index2); // Output: -1
