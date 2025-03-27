141365b8664e4d624c5c7223fb87cf81f9502d3f
salimkhandev <salimt450s@gmail.com>
2025-03-28 00:16:07 +0500

    improved

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
 import App from './App.jsx'
-// import App2 from './App2.jsx' 
 import './index.css'
+// import { BrowserRouter as Router } from 'react-router-dom';
+
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/service-worker.js')
     .then(reg => console.log('Service Worker registered ✅', reg))
     .catch(err => console.log('Service Worker registration failed ❌', err));
 }
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
